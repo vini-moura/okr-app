55e279cb370959d458d458d805b3e8185b6656bf
vini-moura <viniciusoliveira@vivaldi.net>
2022-10-16 15:28:31 -0400

    okr display

--- main.py
+++ main.py
@@ -1,80 +1,48 @@
-import json
 from okr_class import *
+import json
 
-
-with open('json/obj.json','r') as f:
-    obj = json.load(f)
-with open('json/users.json','r') as f:
-    user = json.load(f)
-with open('json/resultadoschave.json','r') as f:
-    rcs = json.load(f)
-
+with open('json/obj.json', 'r') as f:
+    file_obj = json.load(f)
+with open('json/users.json', 'r') as f:
+    file_user = json.load(f)
+with open('json/resultadoschave.json', 'r') as f:
+    file_rcs = json.load(f)
 
 nome = 'vinicius oliveira'
-log_user = [i for i in user if i['nome'] in nome]
-times = [i['time'] for i in user if i['nome'] in nome]
-selecionados = [i for i in obj if i['time'] in times]
+log_user = [i for i in file_user if i['nome'] in nome]
+list_time = [i['time'] for i in file_user if i['nome'] in nome]
+times = [i for group in list_time for i in group]
+selecionados = [i for i in file_obj if i['time'] in times]
+
 
 usuario = []
 for u in log_user:
-    a = User(u['id'],u['nome'],u['time'],u['time'])
+    a = User(u['id'], u['nome'], u['time'], u['time'])
     usuario.append(a)
 
-print(usuario.time)
+objetivos=[Objetivo(o['id'], o['time'], o['setor'], o['obj'], o['responsavel'], o['ano'], o['ciclo']) for o in
+           selecionados]
 
-objetivos = []
-for o in selecionados:
-    # id,time,setor,obj,responsavel,ano,ciclo
-    a = Objetivo(o['id'],o['time'],o['setor'],o['obj'],o['responsavel'],o['ano'],o['ciclo'])
-    objetivos.append(a)
+ids_obj = [o['id'] for o in selecionados]
 
-rc_selecionados = [i for i in rcs if i['id_obj'] in selecionados]
 
+rc_selecionados = [i for i in file_rcs if i['id_obj'] in ids_obj]
 krs = []
-for rc in rcs:
-    #id1, id_obj, setor, texto, tipo, inicial, mudar, meta, responsavel, status, atual, p
-    a = ResultadoChave(rc['id'],    rc['id_obj'],
-                       rc['setor'],     rc['texto'],
-                       rc['tipo'],      rc['inicial'],
-                       rc['mudar'],     rc['meta'],
-                       rc['responsavel'],   rc['status'],
-                       rc['atual'],     rc['p'])
+for rc in rc_selecionados:
+    # id1, id_obj, setor, texto, tipo, inicial, mudar, meta, responsavel, status, atual, p
+    a = ResultadoChave(rc['id'], rc['id_obj'],
+                       rc['setor'], rc['texto'],
+                       rc['tipo'], rc['inicial'],
+                       rc['mudar'], rc['meta'],
+                       rc['responsavel'], rc['status'],
+                       rc['atual'], rc['p'])
     krs.append(a)
 
-media_total = 32
 
-# user_log= 'vinicius oliveira'
-#
-# times_log = [i['time'] for i in user if i['nome'] in user_log]
-# obj_log = [i for i in obj if i['time'] in times_log]
-#
-# obj_mostrar = []
-# for o in obj_log:
-#     # id1,time,setor,obj,responsavel,ano,ciclo
-#     a = Objetivo(o['id'],o['time'],o['setor'],o['obj'],o['responsavel'],o['ano'],o['ciclo'])
-#     obj_mostrar.append(a)
-#
-#
-# print(obj_mostrar[0].obj)
+media_total = 0
+for i in krs:
+    print(f'adicionando: {i.atual}')
+    media_total += i.atual
+    print(f"soma: {media_total}")
 
-
-
-
-
-# for i in user:
-#     a = user[i][user[i]['time'] == user_log]
-#     obj_log.extend(a)
-
-
-# user_log = 'vinicius oliveira'
-# time_log = user['time'][user['nome'] == user_log]
-
-
-# obj_log = []
-# for i in obj:
-#     a = obj[obj['time'] == i].to_dict()
-#     print(a)
-#     b = Objetivo(a['id'],a['time'],a['setor'],a['obj'],a['responsavel'],a['ano'],a['ciclo'])
-#     obj_log.append(b)
-#
-# print(obj_log[0].obj.to_string())
+print(media_total)
--- server.py
+++ server.py
@@ -2,46 +2,43 @@ from flask import Flask, render_template
 from okr_class import *
 import json
 
-
-with open('json/obj.json','r') as f:
-    obj = json.load(f)
-with open('json/users.json','r') as f:
-    user = json.load(f)
-with open('json/resultadoschave.json','r') as f:
-    rcs = json.load(f)
-
+with open('json/obj.json', 'r') as f:
+    file_obj = json.load(f)
+with open('json/users.json', 'r') as f:
+    file_user = json.load(f)
+with open('json/resultadoschave.json', 'r') as f:
+    file_rcs = json.load(f)
 
 nome = 'vinicius oliveira'
-log_user = [i for i in user if i['nome'] in nome]
-times = [i['time'] for i in user if i['nome'] in nome]
-selecionados = [i for i in obj if i['time'] in times]
-
-usuario = []
-for u in log_user:
-    a = User(u['id'],u['nome'],u['time'],u['time'])
-    usuario.append(a)
+log_user = [i for i in file_user if i['nome'] in nome]
+list_time = [i['time'] for i in file_user if i['nome'] in nome]
+times = [i for group in list_time for i in group]
+selecionados = [i for i in file_obj if i['time'] in times]
 
 
-objetivos = []
-for o in selecionados:
-    # id,time,setor,obj,responsavel,ano,ciclo
-    a = Objetivo(o['id'],o['time'],o['setor'],o['obj'],o['responsavel'],o['ano'],o['ciclo'])
-    objetivos.append(a)
+usuario = [User(u['id'], u['nome'], u['time'], u['time']) for u in log_user]
 
-rc_selecionados = [i for i in rcs if i['id_obj'] in selecionados]
+objetivos = [Objetivo(o['id'], o['time'], o['setor'], o['obj'], o['responsavel'], o['ano'], o['ciclo']) for o in
+             selecionados]
+ids_obj = [o['id'] for o in selecionados]
 
+
+rc_selecionados = [i for i in file_rcs if i['id_obj'] in ids_obj]
 krs = []
-for rc in rcs:
-    #id1, id_obj, setor, texto, tipo, inicial, mudar, meta, responsavel, status, atual, p
-    a = ResultadoChave(rc['id'],    rc['id_obj'],
-                       rc['setor'],     rc['texto'],
-                       rc['tipo'],      rc['inicial'],
-                       rc['mudar'],     rc['meta'],
-                       rc['responsavel'],   rc['status'],
-                       rc['atual'],     rc['p'])
+for rc in rc_selecionados:
+    # id1, id_obj, setor, texto, tipo, inicial, mudar, meta, responsavel, status, atual, p
+    a = ResultadoChave(rc['id'], rc['id_obj'],
+                       rc['setor'], rc['texto'],
+                       rc['tipo'], rc['inicial'],
+                       rc['mudar'], rc['meta'],
+                       rc['responsavel'], rc['status'],
+                       rc['atual'], rc['p'])
     krs.append(a)
 
-media_total = 32
+
+media_total = 0
+for i in krs:
+    media_total += i.atual
 
 
 # -------------------------
@@ -50,11 +47,7 @@ app= Flask(__name__)
 
 @app.route('/')
 def home():
-    return render_template("index.html",
-                           media_total = media_total,
-                           objetivos = objetivos,
-                           krs = krs,
-                           usuario= usuario)
-#
+    return render_template("index.html", media_total = media_total, objetivos = objetivos, krs = krs, usuario= usuario)
+
 if __name__ == '__main__':
     app.run(debug=True)
