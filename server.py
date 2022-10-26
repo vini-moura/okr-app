81209b721a839432d0a6e393904e9cea09e3cb25
vini-moura <viniciusoliveira@vivaldi.net>
2022-10-26 11:50:21 -0400

    p. cadastro

--- server.py
+++ server.py
@@ -18,7 +18,7 @@ selecionados = [i for i in file_obj if i['time'] in times]
 
 usuario = [User(u['id'], u['nome'], u['time'], u['time']) for u in log_user]
 
-objetivos = [Objetivo(o['id'], o['time'], o['setor'], o['obj'], o['responsavel'], o['ano'], o['ciclo']) for o in
+objetivos = [Objetivo(o['id'], o['time'], o['setor'], o['texto'], o['responsavel'], o['ano'], o['ciclo']) for o in
              selecionados]
 ids_obj = [o['id'] for o in selecionados]
 
@@ -27,12 +27,12 @@ rc_selecionados = [i for i in file_rcs if i['id_obj'] in ids_obj]
 krs = []
 for rc in rc_selecionados:
     # id1, id_obj, setor, texto, tipo, inicial, mudar, meta, responsavel, status, atual, p
-    a = ResultadoChave(rc['id'], rc['id_obj'],
-                       rc['setor'], rc['texto'],
-                       rc['tipo'], rc['inicial'],
-                       rc['mudar'], rc['meta'],
-                       rc['responsavel'], rc['status'],
-                       rc['atual'], rc['p'])
+    a = ResultadoChave(id1= rc['id'], id_obj=rc['id_obj'], num=rc['num'],
+                       setor=rc['setor'], texto= rc['texto'],
+                       tipo=rc['tipo'], inicial= rc['inicial'],
+                       mudar=rc['mudar'], meta= rc['meta'],
+                       responsavel= rc['responsavel'], status= rc['status'],
+                       atual=rc['atual'], p= rc['p'])
     krs.append(a)
 
 
@@ -42,12 +42,23 @@ for i in krs:
 
 
 # -------------------------
-
 app= Flask(__name__)
 
 @app.route('/')
 def home():
     return render_template("index.html", media_total = media_total, objetivos = objetivos, krs = krs, usuario= usuario)
 
+@app.route('/cadastrar')
+def cadastrar():
+    return render_template("cadastrar.html", usuario= usuario)
+
+@app.route('/login', method=['POST'])
+def receive_data():
+    #do something
+    pass
+
 if __name__ == '__main__':
     app.run(debug=True)
+
+#recorte pop do ms
+#recorte das violencias
